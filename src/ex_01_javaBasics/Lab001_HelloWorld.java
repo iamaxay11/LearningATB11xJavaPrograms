f11d2d4532180d3b264d9c685eccee7cfa09514e
Akshay Chavan <akshay.chavan@fortris.com>
2025-03-10 21:40:50 +0530

    10 March committ

--- src/ex_01_javaBasics/Lab001_HelloWorld.java
+++ src/ex_01_javaBasics/Lab001_HelloWorld.java
@@ -1,6 +1,6 @@
 package ex_01_javaBasics;
 
-public class HelloWorld {
+public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
