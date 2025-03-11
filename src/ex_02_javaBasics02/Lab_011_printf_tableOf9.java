8e2bd10d5fcdfb3108db20688fa55becdd18c1e0
akshay.chavan <akshay.chavan@fortris.com>
2025-03-11 10:23:51 +0530

    11 March committ

--- src/ex_02_javaBasics02/Lab_011_printf_tableOf9.java
+++ src/ex_02_javaBasics02/Lab_011_printf_tableOf9.java
@@ -6,22 +6,22 @@ public class Lab_011_printf_tableOf9 {
 
         System.out.printf("%d * 1 = 9",table);
         System.out.println();
-        System.out.printf("%d * 2 = 18",table);
+        System.out.printf("%d * 2 = %d",table,table*2);
         System.out.println();
-        System.out.printf("%d * 3 = 27",table);
+        System.out.printf("%d * 3 = %d",table,table*3);
         System.out.println();
-        System.out.printf("%d * 4 = 36",table);
+        System.out.printf("%d * 4 = %d",table,table*4);
         System.out.println();
-        System.out.printf("%d * 5 = 45",table);
+        System.out.printf("%d * 5 = %d",table,table*5);
         System.out.println();
-        System.out.printf("%d * 6 = 54",table);
+        System.out.printf("%d * 6 = %d",table,table*6);
         System.out.println();
-        System.out.printf("%d * 7 = 63",table);
+        System.out.printf("%d * 7 = %d",table,table*7);
         System.out.println();
-        System.out.printf("%d * 8 = 72",table);
+        System.out.printf("%d * 8 = %d",table,table*8);
         System.out.println();
-        System.out.printf("%d * 9 = 81",table);
+        System.out.printf("%d * 9 = %d",table,table*9);
         System.out.println();
-        System.out.printf("%d * 10 = 90",table);
+        System.out.printf("%d * 10 = %d",table,table*10);
     }
 }
