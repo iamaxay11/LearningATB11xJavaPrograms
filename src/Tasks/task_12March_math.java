930d7fa7151440f7e0e7394857f218b8d162e991
iamaxay11 <akshaychavan1102@gmail.com>
2025-03-12 13:37:06 +0530

    12 March committ

---
+++ src/Tasks/task_12March_math.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class task_12March_math {
+    public static void main(String[] args) {
+        int x=10;
+        int y=10;
+        int z=10;
+
+        System.out.println(Math.cbrt((Math.pow(x,2))+Math.pow(y,2)-Math.abs(z)));
+    }
+
+}
